b325e15b8431e7caaf98dd8efa0ad30c741afcb0
Diego Giraldo <diego.giraldo@moonpig.com>
2020-09-16 16:18:27 +0000

    final

---
+++ models/final/customers.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+select
+  c.customer_id,
+  , c.first_name
+  , c.last_name
+  , co.first_order_date
+  , co.most_recent_order_date
+  , coalesce(co.number_of_orders, 0) as number_of_orders
+  , coalesce(lifetimevalue,0) as lifetimevalue
+
+from {{ ref('stg_customers') }} c
+left join  {{ ref('stg_customer_orders') }} co 
+on  c.customer_id = co.customer_id
