665ac9dc35893a1e13435100d0b27b4993830af7
Diego Giraldo <diego.giraldo@moonpig.com>
2020-09-16 16:10:32 +0000

    final

--- models/staging/stg_customer_orders.sql
+++ models/staging/stg_customer_orders.sql
@@ -4,6 +4,7 @@ select
     max(order_date) as most_recent_order_date,
     count(order_id) as number_of_orders,
     sum(amount) as lifetimevalue
+    
 from {{ ref('orders') }}
 where payment_status = 'success'
 group by 1
