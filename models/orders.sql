1e4da6740d769c51bff9b0c58844758ed4b3b9d7
Diego Giraldo <diego.giraldo@moonpig.com>
2020-09-16 15:27:57 +0000

    orders

---
+++ models/orders.sql
@@ -0,0 +1,22 @@
+with orders as (
+  select * 
+  from {{ ref('stg_orders') }}
+),
+
+payments as (
+  select * 
+  from {{ ref('stg_payments') }}
+),
+
+final as(
+  select 
+    o.order_id,
+    o.customer_id,
+    p.amount 
+  from orders o 
+  join payments p 
+  on o.order_id = p.order_id
+)
+
+select *
+from final
